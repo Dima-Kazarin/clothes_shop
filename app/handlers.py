51a0209111f521a327d5c4b7bf2c8fa7e2cbb491
Dima <dimakazarin66@gmail.com>
2025-04-11 10:19:42 +0300

    added categories functional

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,5 @@
 from aiogram import F, Router
-from aiogram.filters import CommandStart, Command
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.filters import CommandStart
 
 import app.keyboard as kb
 
@@ -11,3 +10,55 @@ router = Router()
 async def start(message):
     await message.answer('Hello', reply_markup=kb.main)
 
+
+@router.message(F.text == 'Catalog')
+async def handle_catalog(message):
+    await message.answer('What products would you like to see?', reply_markup=kb.filter_buttons)
+
+
+@router.callback_query(F.data == 'categories')
+async def show_categories(callback):
+    await callback.message.edit_text('Categories', reply_markup=kb.categories_buttons)
+
+
+@router.callback_query(F.data == 'to_main')
+async def to_main(callback):
+    await callback.message.edit_text('What products would you like to see?', reply_markup=kb.filter_buttons)
+
+
+@router.callback_query(F.data == 'to_categories')
+async def to_categories(callback):
+    await callback.message.edit_text('Categories', reply_markup=kb.categories_buttons)
+
+
+@router.callback_query(F.data == 'outerwear')
+async def show_outerwear(callback):
+    await callback.message.edit_text('Select your outerwear:', reply_markup=await kb.show_outerwear())
+
+
+@router.callback_query(F.data == 'underwear')
+async def show_underwear(callback):
+    await callback.message.edit_text('Select your underwear:', reply_markup=await kb.show_underwear())
+
+
+@router.callback_query(F.data == 'footwear')
+async def show_footwear(callback):
+    await callback.message.edit_text('Select your footwear:', reply_markup=await kb.show_footwear())
+
+
+@router.callback_query(F.data.startswith('outerwear_'))
+async def show_good_outerwear(callback):
+    good = callback.data.split('_')[1]
+    await callback.message.edit_text(good)
+
+
+@router.callback_query(F.data.startswith('underwear_'))
+async def show_good_underwear(callback):
+    good = callback.data.split('_')[1]
+    await callback.message.edit_text(good)
+
+
+@router.callback_query(F.data.startswith('footwear_'))
+async def show_good_footwear(callback):
+    good = callback.data.split('_')[1]
+    await callback.message.edit_text(good)
--- app/keyboard.py
+++ app/keyboard.py
@@ -1,6 +1,49 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+outerwears = ['T1', 'T2', 'T3']
+underwears = ['T4', 'T5', 'T6']
+footwears = ['T7', 'T8', 'T9']
+
 main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Каталог')]
+    [KeyboardButton(text='Catalog')]
 ], resize_keyboard=True)
+
+filter_buttons = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='All', callback_data='all_goods')],
+    [InlineKeyboardButton(text='Categories', callback_data='categories')]
+])
+
+categories_buttons = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Outerwear', callback_data='outerwear')],
+    [InlineKeyboardButton(text='Underwear', callback_data='underwear')],
+    [InlineKeyboardButton(text='Footwear', callback_data='footwear')],
+    [InlineKeyboardButton(text='◀️', callback_data='to_main')],
+])
+
+
+async def show_outerwear():
+    keyboard = InlineKeyboardBuilder()
+    for good in outerwears:
+        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'outerwear_{good}'))
+    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
+
+    return keyboard.adjust(1).as_markup()
+
+
+async def show_underwear():
+    keyboard = InlineKeyboardBuilder()
+    for good in underwears:
+        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'underwear_{good}'))
+    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
+
+    return keyboard.adjust(1).as_markup()
+
+
+async def show_footwear():
+    keyboard = InlineKeyboardBuilder()
+    for good in footwears:
+        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'footwear_{good}'))
+    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
+
+    return keyboard.adjust(1).as_markup()
