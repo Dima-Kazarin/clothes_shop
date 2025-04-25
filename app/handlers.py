f009b4656ca29770fd736e87aec12534f2e2f301
Dima <dimakazarin66@gmail.com>
2025-04-25 11:53:55 +0300

    add products

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,10 @@
+import requests
 from aiogram import F, Router
 from aiogram.filters import CommandStart
+from aiogram.types import InputMediaPhoto, InputFile, URLInputFile
 
 import app.keyboard as kb
+from products import products
 
 router = Router()
 
@@ -26,6 +29,12 @@ async def to_main(callback):
     await callback.message.edit_text('What products would you like to see?', reply_markup=kb.filter_buttons)
 
 
+@router.callback_query(F.data == 'from_good_to_main')
+async def from_good_to_main(callback):
+    await callback.message.delete()
+    await callback.message.answer('What products would you like to see?', reply_markup=kb.filter_buttons)
+
+
 @router.callback_query(F.data == 'to_categories')
 async def to_categories(callback):
     await callback.message.edit_text('Categories', reply_markup=kb.categories_buttons)
@@ -33,32 +42,60 @@ async def to_categories(callback):
 
 @router.callback_query(F.data == 'outerwear')
 async def show_outerwear(callback):
-    await callback.message.edit_text('Select your outerwear:', reply_markup=await kb.show_outerwear())
+    await callback.message.edit_text('Select your outerwear:', reply_markup=await kb.show_outerwear(page=0))
 
 
 @router.callback_query(F.data == 'underwear')
 async def show_underwear(callback):
-    await callback.message.edit_text('Select your underwear:', reply_markup=await kb.show_underwear())
+    await callback.message.edit_text('Select your underwear:', reply_markup=await kb.show_underwear(page=0))
 
 
 @router.callback_query(F.data == 'footwear')
 async def show_footwear(callback):
-    await callback.message.edit_text('Select your footwear:', reply_markup=await kb.show_footwear())
+    await callback.message.edit_text('Select your footwear:', reply_markup=await kb.show_footwear(page=0))
 
 
-@router.callback_query(F.data.startswith('outerwear_'))
+@router.callback_query(F.data.startswith('good_'))
 async def show_good_outerwear(callback):
-    good = callback.data.split('_')[1]
-    await callback.message.edit_text(good)
+    await callback.message.delete()
+
+    good_id = callback.data.split('_')[1]
+    for product in products:
+        if product['name'].endswith(good_id):
+            good = product
+
+    message_text = f'{" ".join(good["name"].split(" ")[:-1])}\n'
+    message_text += f'Розміри: {", ".join(good["sizes"])}\n'
+    message_text += f'Ціна: {good["price"]}\n'
+
+    photo = URLInputFile(good['photo'])
+    await callback.message.answer_photo(photo, caption=message_text, reply_markup=kb.good_buttons)
 
 
-@router.callback_query(F.data.startswith('underwear_'))
-async def show_good_underwear(callback):
-    good = callback.data.split('_')[1]
-    await callback.message.edit_text(good)
+@router.callback_query(F.data == 'all_goods')
+async def show_all_goods_handler(callback):
+    await callback.message.edit_text('All goods:', reply_markup=await kb.show_all_goods(page=0))
 
 
-@router.callback_query(F.data.startswith('footwear_'))
-async def show_good_footwear(callback):
-    good = callback.data.split('_')[1]
-    await callback.message.edit_text(good)
+@router.callback_query(F.data.startswith('page_'))
+async def paginate_goods(callback):
+    page = int(callback.data.split('_')[1])
+    await callback.message.edit_text('All goods:', reply_markup=await kb.show_all_goods(page=page))
+
+
+@router.callback_query(F.data.startswith('outerwearpage_'))
+async def paginate_outwear(callback):
+    page = int(callback.data.split('_')[1])
+    await callback.message.edit_text('Select your outerwear:', reply_markup=await kb.show_outerwear(page=page))
+
+
+@router.callback_query(F.data.startswith('underwearpage_'))
+async def paginate_underwear(callback):
+    page = int(callback.data.split('_')[1])
+    await callback.message.edit_text('Select your underwear:', reply_markup=await kb.show_underwear(page=page))
+
+
+@router.callback_query(F.data.startswith('footwearpage_'))
+async def paginate_footwear(callback):
+    page = int(callback.data.split('_')[1])
+    await callback.message.edit_text('Select your footwear:', reply_markup=await kb.show_footwear(page=page))
--- app/keyboard.py
+++ app/keyboard.py
@@ -1,9 +1,8 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from products import products
 
-outerwears = ['T1', 'T2', 'T3']
-underwears = ['T4', 'T5', 'T6']
-footwears = ['T7', 'T8', 'T9']
+ITEMS_PER_PAGE = 5
 
 main = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text='Catalog')]
@@ -21,29 +20,141 @@ categories_buttons = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text='◀️', callback_data='to_main')],
 ])
 
+good_buttons = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='◀️', callback_data='from_good_to_main'),
+     InlineKeyboardButton(text='🛍️', callback_data='add_to_cart')],
+])
 
-async def show_outerwear():
+
+async def show_outerwear(page):
+    outerwears = []
+    for product in products:
+        if product['name'].lower().startswith('худі'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('світшот'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('футболка'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('куртка'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('пуховик'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('вітровка'):
+            outerwears.append(product)
+        elif product['name'].lower().startswith('бомбер'):
+            outerwears.append(product)
+
     keyboard = InlineKeyboardBuilder()
-    for good in outerwears:
-        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'outerwear_{good}'))
-    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
 
-    return keyboard.adjust(1).as_markup()
+    start = page * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    goods_slice = outerwears[start:end]
+
+    for good in goods_slice:
+        keyboard.add(InlineKeyboardButton(text=good['name'], callback_data=f'good_{good["name"].split(" ")[-1]}'))
+
+    keyboard.adjust(1)
+
+    nav_buttons = []
+    if page > 0:
+        nav_buttons.append(InlineKeyboardButton(text='◀️', callback_data=f'outerwearpage_{page - 1}'))
+    nav_buttons.append(InlineKeyboardButton(text='🏠', callback_data='to_categories'))
+    if end < len(outerwears):
+        nav_buttons.append(InlineKeyboardButton(text='▶️', callback_data=f'outerwearpage_{page + 1}'))
+
+    if nav_buttons:
+        keyboard.row(*nav_buttons)
+
+    return keyboard.as_markup()
 
 
-async def show_underwear():
+async def show_underwear(page):
+    underwears = []
+    for product in products:
+        if product['name'].lower().startswith('джинси'):
+            underwears.append(product)
+        elif product['name'].lower().startswith('шорти'):
+            underwears.append(product)
+
     keyboard = InlineKeyboardBuilder()
-    for good in underwears:
-        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'underwear_{good}'))
-    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
 
-    return keyboard.adjust(1).as_markup()
+    start = page * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    goods_slice = underwears[start:end]
+
+    for good in goods_slice:
+        keyboard.add(InlineKeyboardButton(text=good['name'], callback_data=f'good_{good["name"].split(" ")[-1]}'))
+
+    keyboard.adjust(1)
+
+    nav_buttons = []
+    if page > 0:
+        nav_buttons.append(InlineKeyboardButton(text='◀️', callback_data=f'underwearpage_{page - 1}'))
+    nav_buttons.append(InlineKeyboardButton(text='🏠', callback_data='to_categories'))
+    if end < len(underwears):
+        nav_buttons.append(InlineKeyboardButton(text='▶️', callback_data=f'underwearpage_{page + 1}'))
+
+    if nav_buttons:
+        keyboard.row(*nav_buttons)
+
+    return keyboard.as_markup()
 
 
-async def show_footwear():
+async def show_footwear(page):
+    footwears = []
+    for product in products:
+        if product['name'].lower().startswith('кеди'):
+            footwears.append(product)
+        elif product['name'].lower().startswith('кросівки'):
+            footwears.append(product)
+        elif product['name'].lower().startswith('тапочки'):
+            footwears.append(product)
+
     keyboard = InlineKeyboardBuilder()
-    for good in footwears:
-        keyboard.add(InlineKeyboardButton(text=good, callback_data=f'footwear_{good}'))
-    keyboard.add(InlineKeyboardButton(text='◀️', callback_data='to_categories'))
 
-    return keyboard.adjust(1).as_markup()
+    start = page * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    goods_slice = footwears[start:end]
+
+    for good in goods_slice:
+        keyboard.add(InlineKeyboardButton(text=good['name'], callback_data=f'good_{good["name"].split(" ")[-1]}'))
+
+    keyboard.adjust(1)
+
+    nav_buttons = []
+    if page > 0:
+        nav_buttons.append(InlineKeyboardButton(text='◀️', callback_data=f'footwearpage_{page - 1}'))
+    nav_buttons.append(InlineKeyboardButton(text='🏠', callback_data='to_categories'))
+    if end < len(footwears):
+        nav_buttons.append(InlineKeyboardButton(text='▶️', callback_data=f'footwearpage_{page + 1}'))
+
+    if nav_buttons:
+        keyboard.row(*nav_buttons)
+
+    return keyboard.as_markup()
+
+
+async def show_all_goods(page):
+    all_goods = products
+    keyboard = InlineKeyboardBuilder()
+
+    start = page * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+    goods_slice = all_goods[start:end]
+
+    for good in goods_slice:
+        keyboard.add(InlineKeyboardButton(text=good['name'], callback_data=f'good_{good["name"].split(" ")[-1]}'))
+
+    keyboard.adjust(1)
+
+    nav_buttons = []
+    if page > 0:
+        nav_buttons.append(InlineKeyboardButton(text='◀️', callback_data=f'page_{page - 1}'))
+    nav_buttons.append(InlineKeyboardButton(text='🏠', callback_data='to_main'))
+    if end < len(all_goods):
+        nav_buttons.append(InlineKeyboardButton(text='▶️', callback_data=f'page_{page + 1}'))
+
+    if nav_buttons:
+        keyboard.row(*nav_buttons)
+
+    return keyboard.as_markup()
